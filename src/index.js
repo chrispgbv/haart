472a9df64a9a08400f3fab3359e626abf259ec7d
chrispgbv <35257362+chrispgbv@users.noreply.github.com>
2020-03-30 13:31:04 +0200

    added function isde axis and grid, for lesser code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 import _ from 'lodash';
-import {getCanvas} from '../lib';
-import {outer} from '../lib';
-import {grid} from '../lib';
-import {axis} from '../lib';
+import { getCanvas } from '../lib';
+import { outer } from '../lib';
+import { grid } from '../lib';
+import { axis } from '../lib';
+import { sineCurve1 } from '../lib';
 
 getCanvas('myCanvas');
 
 outer(getCanvas('myCanvas'),
-    '#e4e7f5', 
+    '#e4e7f5',
     'axisxm'
 )
 
 grid(getCanvas('myCanvas'),
     '#4159a1',
-    'dotted'
+    'dotx'
 )
 
 axis(
-    getCanvas('myCanvas'),
-    'middle',
-    'middle'
+    getCanvas('myCanvas'),'middle','middle'
 )
+
+//creates sine curve, ctx =  canvas, step= time , 50 = height of graph
+let step = 0
+step += 3;
+
+sineCurve1(
+    getCanvas('myCanvas'),
+    step,
+    50,
+    30
+);
+
+
+
+
